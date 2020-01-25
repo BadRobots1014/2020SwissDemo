be96609632249bc2fa71f00de084e7c49fdc50fb
SomeoneMysterious <ilovebadgerswill@gmail.com>
2020-01-25 10:27:49 -0500

    Finished Ultrasonic Sensor

--- src/main/java/frc/robot/subsystems/UltrasonicSubsystem.java
+++ src/main/java/frc/robot/subsystems/UltrasonicSubsystem.java
@@ -10,7 +10,7 @@ public class UltrasonicSubsystem extends SubsystemBase {
 
     private static final int kUltrasonicPort = 0;
     //conversion coefficient
-    private static final double toInches = 1.0;
+    private static final double toInches = 0.04139;
 
     private final AnalogInput m_ultrasonic = new AnalogInput(kUltrasonicPort);
 
@@ -20,8 +20,8 @@ public class UltrasonicSubsystem extends SubsystemBase {
     }
 
     public void updateDashboard(){
-        SmartDashboard.putNumber("Ultrasonic Value", m_ultrasonic.getVoltage());
-        SmartDashboard.putNumber("Distance", m_ultrasonic.getVoltage()*toInches);
+        SmartDashboard.putNumber("Ultrasonic Value", m_ultrasonic.getValue());
+        SmartDashboard.putNumber("Distance", m_ultrasonic.getValue()*toInches);
     }
 
 
